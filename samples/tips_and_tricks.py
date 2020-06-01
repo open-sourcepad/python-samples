8de26be1ebaa5afb34d08319946fc839c25a254e
dyao <djy@sourcepad.com>
2020-06-01 17:49:46 +0800

    added tips and tricks

---
+++ samples/tips_and_tricks.py
@@ -0,0 +1,23 @@
+class Test:
+    pass
+
+
+def attr(self):
+    print('123')
+
+setattr(Test, 'attr', attr)
+
+Test().attr()
+
+setattr(Test, 'test_data', 'sample test data')
+
+print(getattr(Test, 'test_data1', None))
+
+kwargs = {'a': 1}
+print(kwargs.get('a'))
+
+dir(Test)
+# ipython
+
+# class explanation
+# import pry; pry()
